503a3ec574c778e2fe9d6c1f8694c4540edb3c4c
Sadiq <sadiqy93@gmail.com>
2018-04-15 00:32:46 -0600

    updated server

--- server.js
+++ server.js
@@ -3,23 +3,41 @@
  *
  */
 
-var game = require('./game');
-let express = require("express");
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
-var path = require('path');
+const game = require('./game');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 3000;
+const path = require('path');
 
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const passport = require('passport')
+const dbUtils = require('./src/utils/database')
+require('dotenv').config()
+
+require('./src/utils/passport')(passport)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true
+}))
+
+app.use(flash())
+app.use(passport.initialize())
+app.set('view engine', 'ejs')
+app.use(passport.session())
 
 var users = []; // list of users (string) (currently unused)
-
-app.get('/', function(req, res)
-{
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.use(express.static(path.join(__dirname, "public")));
 
 io.on('connection', function (socket)
 {
@@ -55,3 +73,16 @@ server.listen(port, function ()
 {
     console.log('listening on *:' + port);
 });
+
+// connect to the db
+
+const url = `mongodb://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASS}${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_NAME}`
+dbUtils.connect(url)
+    .then(_ => {
+        console.log('Successfully connected to DB 😀')
+    })
+    .catch(err => {
+        console.log(`Error Connecting to the mongo DB ${error}`)
+    })
+
+require('./src/routes/routes')(app, passport)
